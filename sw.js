ad59598a5afc66632c628b3de93d41ef9999ff4c
bert <bertmaosong@gmail.com>
2017-02-21 16:38:00 +0800

    update

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-var cacheVersion = 1;
+'use strict';
+
+var cacheVersion = 0;
 var currentCache = {
     offline: 'offline-cache' + cacheVersion
 };
-const offlineUrl = 'offline-page.html';
+const offlineUrl = 'offline.html';
 
 this.addEventListener('install', event => {
     event.waitUntil(
         caches.open(currentCache.offline).then(function(cache) {
             return cache.addAll([
-                './img/offline.svg',
+                './offline.svg',
                 offlineUrl
             ]);
         })
@@ -16,21 +18,15 @@ this.addEventListener('install', event => {
 });
 
 this.addEventListener('fetch', event => {
-    // request.mode = navigate isn't supported in all browsers
-    // request.mode = naivgate 并没有得到所有浏览器的支持
-    // so include a check for Accept: text/html essay-header.
-    // 因此对 essay-header 的 Accept：text/html 进行核实
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.essay - headers.get('accept').includes('text/html'))) {
+
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
         event.respondWith(
             fetch(event.request.url).catch(error => {
                 // Return the offline page
-                // 返回离线页面
                 return caches.match(offlineUrl);
             })
         );
     } else {
-        // Respond with everything else if we can
-        // 返回任何我们能返回的东西
         event.respondWith(caches.match(event.request)
             .then(function(response) {
                 return response || fetch(event.request);
