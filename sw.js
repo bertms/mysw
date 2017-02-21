5133e727eeec418d1efd7367736062e7dbc1ff0f
bert <bertmaosong@gmail.com>
2017-02-21 15:15:17 +0800

    update

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@
  * @version $Id$
  */
 // The files we want to cache
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     '/mysw/',
     '/mysw/css/style.css',
@@ -14,4 +15,11 @@ var urlsToCache = [
 // Set the callback for the install step
 self.addEventListener('install', function(event) {
     // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
