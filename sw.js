d86686e5107e9e512292296dd4361dccce74e1b8
bert <bertmaosong@gmail.com>
2017-02-21 15:11:30 +0800

    update

--- sw.js
+++ sw.js
@@ -4,3 +4,14 @@
  * @date    2017-02-21 14:28:16
  * @version $Id$
  */
+// The files we want to cache
+var urlsToCache = [
+    '/mysw/',
+    '/mysw/css/style.css',
+    '/mysw/images/2.jpg'
+];
+
+// Set the callback for the install step
+self.addEventListener('install', function(event) {
+    // Perform install steps
+});
